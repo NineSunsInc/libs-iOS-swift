9b78a79aada92ef6c0a71a1211a94a7d7a2a049b
Luan Tran <luan.tran@c0x12c.com>
2024-08-21 09:44:01 +0700

    Initial Commit

---
+++ ninesunsencryptedTests/ninesunsencryptedTests.swift
@@ -0,0 +1,17 @@
+//
+//  ninesunsencryptedTests.swift
+//  ninesunsencryptedTests
+//
+//  Created by Luan Tran on 21/8/24.
+//
+
+import Testing
+@testable import ninesunsencrypted
+
+struct ninesunsencryptedTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
